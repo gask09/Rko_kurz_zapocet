0d24c7ba1f6ed6b7f21cd0dc1fc39d3411dd79eb
Přemysl Baláž <87470058+gask09@users.noreply.github.com>
2025-09-05 11:43:36 +0200

    Add files via upload

--- plastome_genes.R
+++ plastome_genes.R
@@ -1,16 +1,42 @@
+if (!require("rentrez")) {
+  install.packages("rentrez")
+}
+
+if (!require("glue")) {
+  install.packages("glue")
+}
+
+if (!require("ape")) {
+  install.packages("ape")
+}
+
+if (!require("stringr")) {
+  install.packages("rentrez")
+}
+
+if (!require("pheatmap")) {
+  install.packages("pheatmap")
+}
+
 library(rentrez)
 library(glue)
 library(ape)
 library(stringr)
-library(ggplot2)
-install.packages("pheatmap")
 library(pheatmap)
+
+#______________________Variables that can be modified__________________________
+
 # !!! Set this variable according to your preferences !!!
-OUTPUT_DIR_FOR_PLASTOMES="~/Desktop/paph_annotated_plastomes"
+OUTPUT_DIR_FOR_PLASTOMES="~/Desktop/annotated_plastomes"
 
 if (!dir.exists(OUTPUT_DIR_FOR_PLASTOMES)) {
   dir.create(OUTPUT_DIR_FOR_PLASTOMES)
 }
+# !!! Set this variable according to your preferences. Make sure it is written correctly
+# Otherwise the script will fail at some point !!!
+TAXON_TO_SEARCH_FOR <- "Pleurothallidinae"
+
+#______________________Function definitions____________________________________
 
 # Queries the database for IDs of complete plastomes
 # Override plastome sizes only if you know that they differ 
@@ -41,43 +67,52 @@ annotate_w_chloe <- function(fasta_path,
   system(parsed_command)
 }
 
-get_gene_from_attribute <- function(gff_attribute) {
+parse_attribute <- function(gff_attribute) {
   split_attribute <- strsplit(x=gff_attribute, split=";")[[1]][3]
   gene_name <- strsplit(split_attribute, "=")[[1]][2]
   return(gene_name)
 }
 
 
-ids <- query_nucleotide_gb("Paphiopedilum")
+#________________________________Script itself___________________________________
+
+ids <- query_nucleotide_gb(TAXON_TO_SEARCH_FOR)
 
 for (id in ids) {
   get_fasta(id)
   annotate_w_chloe(fasta_path = glue("/tmp/plastome_seq_{id}.fasta"), output_name = OUTPUT_DIR_FOR_PLASTOMES)
 }
-
+# Just an empty list to hold vectors of gene names
 list_of_individuals_with_all_genes <- list()
 
+# All the magic happens here
 for (filename in dir(OUTPUT_DIR_FOR_PLASTOMES, patter="*.gff")) {
   loaded_gff <- ape::read.gff(glue("{OUTPUT_DIR_FOR_PLASTOMES}/{filename}"))
   gff_genes_only <- loaded_gff[loaded_gff$type=="gene",]
   gff_attributes <- gff_genes_only$attributes
   gene_names <- c()
   for (attribute in gff_attributes) {
-    gene_name <- get_gene_from_attribute(attribute)
+    gene_name <- parse_attribute(attribute)
     gene_names <- append(gene_names, gene_name)
   }
-  print(gene_names)
   ncbi_entry_id <- stringr::str_remove(filename ,".chloe.gff")
-  print(ncbi_entry_id)
   list_of_individuals_with_all_genes[[ncbi_entry_id]] <- gene_names
 }
 
+# Unique set of all genes, definitely not the smartest way to do this, some genes might be missing if they are missing in all plastomes
 all_genes <- unique(unlist(list_of_individuals_with_all_genes))
 
-test <- sapply(list_of_individuals_with_all_genes, function(genes) {
+# Maps all present genes for each sample on a reference and recovers number of copies
+# rps12 is transsplicing and thus occurs ~4x in genome
+genes_presence_table <- sapply(list_of_individuals_with_all_genes, function(genes) {
   table(factor(genes, levels = all_genes))
 })
 
-row.names(test) <- all_genes
+# Appends row names and plots a heatmap
+row.names(genes_presence_table) <- all_genes
+rownames(genes_presence_table)
+genes_presence_table_reordered <- genes_presence_table[order(rownames(genes_presence_table)),]
 
-pheatmap(test,cluster_rows = FALSE, cluster_cols = FALSE, border_color = "black")
+
+pheatmap(t(genes_presence_table_reordered), cluster_rows = FALSE, cluster_cols = FALSE)
+
